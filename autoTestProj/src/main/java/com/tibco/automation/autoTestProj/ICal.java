0dad6c8be6aa5520f7261075739b6d4c7218229a
meet2san2012 <meet2san2012@gmail.com>
2016-05-06 05:57:44 -0700

    initial checkin

---
+++ autoTestProj/src/main/java/com/tibco/automation/autoTestProj/ICal.java
@@ -0,0 +1,11 @@
+package com.tibco.automation.autoTestProj;
+
+public interface ICal {
+	int add(int a, int b);
+
+	int sub(int a, int b);
+
+	int mul(int a, int b);
+
+	int div(int a, int b) throws Exception;
+}
